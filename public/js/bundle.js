cce066d25495050b1bb853df6ce62fc6d825d6c3
Hitsuga Aorusaki <apujols17@lawrenceville.org>
2016-07-30 18:06:37 -0400

    fixed score

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -457,12 +457,11 @@ function update(dt){
   }
   // Check collision with lightsaber and enemy at every iteration
   //ALSO updates "score" to document each time enemy is hit
-var score = 0;
 Utils.checkCollision(lightsaber.children[0], "enemy", true, function(result){
   	if(result){
   		socket.emit('sendhit');
   		result.velocity = new THREE.Vector3(1, 0, 0);
-  		score++;
+  		score+=1;
   		document.getElementById("score").innerHTML = score;
   	}
   });
