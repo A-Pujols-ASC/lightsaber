8beb4a4a90e12bc09036a601637c189c34a4d703
Hitsuga Aorusaki <apujols17@lawrenceville.org>
2016-07-30 22:36:00 -0400

    absolute madman approach at creaitng html elemnts at load

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -21,7 +21,12 @@ function changeRed(){
 		document.getElementById("greenSaber").style["background-color"] = "";
 		color = "#ff0000";
 }
-		
+//Increments our timer by 1 second
+var timer = setInterval(function(){increment()},1000);
+function increment(){
+    timer++;
+    document.getElementById("timer").innerHTML = timer; 	
+}
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 function Corridor(textureLoader){
 
