0bb3fc4c09354b67189609fc3689358b190059d2
adonispujols <adonispujols@users.noreply.github.com>
2017-06-14 13:26:56 -0400

    Moving Lightsaber Sound Effects to Viewer

--- public/js/lightsaber.js
+++ public/js/lightsaber.js
@@ -72,7 +72,5 @@ function changeAudio(){
 }
 
 socket.on('playsound', function(data){
-    var hitSound = new Audio("/sounds/hit1.wav")
-    hitSound.play();
 	// hitSounds[Math.floor(Math.random() * 3)].play();
 });
